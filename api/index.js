8da7542ac181e3d495f15221fc5a9c2caf1d35f6
OssFit <oalatrista@gmail.com>
2023-02-04 22:24:07 -0500

    deployeando back corrgido index.js

--- api/index.js
+++ api/index.js
@@ -20,10 +20,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
+const PORT=process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at process.env.PORT'); // eslint-disable-line no-console
   });
 });
