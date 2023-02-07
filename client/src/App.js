5631b5e248cbf4e315af7198abde46774c2141a1
OssFit <oalatrista@gmail.com>
2023-02-07 12:00:31 -0500

    Loading agregado

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {Route} from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
 import { LandingPage } from './components/LandingPage/LandingPage';
 import { Home } from './components/Home/Home';
 import CreateForm from './components/CreatePokemon/CreatePokemon';
@@ -11,11 +10,11 @@ axios.defaults.baseURL="https://pi-pokemon-main-production-20c7.up.railway.app/"
 
 
 function App() {
-  const history=useHistory()
+
   return (
   
     <div className="App">
-      <Route path='/404' component={NotFound} />
+      
       <Route exact path={'/'}>
         <LandingPage />
       </Route>
@@ -24,9 +23,7 @@ function App() {
       </Route>
       <Route exact path={'/create'} component={CreateForm}/>
       <Route exact path={`/home/:id`} component={Detail}/>
-      <Route path='*'>
-        <NotFound></NotFound>
-      </Route>
+      
     </div>
   
   );
